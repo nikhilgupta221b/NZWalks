47cda923c39a5ee6749971d40cb6f4980b9899b3
Nikhil <nikhilgupta304@gmail.com>
2025-02-10 21:11:54 +0530

    "Added Migrations"

---
+++ NZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions) {
+            
+        }
+
+        DbSet<Walk> Walks { get; set; }
+        DbSet<Difficulty> Difficulty { get; set; }
+        DbSet<Region> Region { get; set; }
+    }
+}
