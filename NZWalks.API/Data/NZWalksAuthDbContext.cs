5d5f8f367cc43e3770be0b54386a215ef7f4cb8a
nikhil <nikhilgupta304@gmail.com>
2025-02-25 22:20:03 +0530

    Added Auth Db Context File

---
+++ NZWalks.API/Data/NZWalksAuthDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NZWalks.API.Data;
+
+public class NZWalksAuthDbContext : IdentityDbContext
+{
+    public NZWalksAuthDbContext(DbContextOptions<NZWalksAuthDbContext> options) : base(options)
+    {
+    }
+}
