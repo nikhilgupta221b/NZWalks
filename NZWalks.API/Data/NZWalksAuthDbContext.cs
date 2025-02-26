cd93fb6383df0b4b397b6ef7194c469aebdeb6f6
nikhil <nikhilgupta304@gmail.com>
2025-02-26 19:05:16 +0530

    Seeded Data into Identity Db

--- NZWalks.API/Data/NZWalksAuthDbContext.cs
+++ NZWalks.API/Data/NZWalksAuthDbContext.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,5 +8,32 @@ public class NZWalksAuthDbContext : IdentityDbContext
 {
     public NZWalksAuthDbContext(DbContextOptions<NZWalksAuthDbContext> options) : base(options)
     {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        var readerRoleId = "3F2504E0-4F89-41D3-9A0C-0305E82C3301";
+        var writerRoleId = "A6F2C90B-0D5A-4F77-925D-1D81C03D8F59";
+
+        var roles = new List<IdentityRole>
+        {
+            new IdentityRole
+            {
+                Id = readerRoleId,
+                ConcurrencyStamp = readerRoleId,
+                Name = readerRoleId,
+                NormalizedName = readerRoleId.ToUpper()
+            },
+            new IdentityRole
+            {
+                Id = writerRoleId,
+                ConcurrencyStamp = writerRoleId,
+                Name = writerRoleId,
+                NormalizedName = writerRoleId.ToUpper()
+            }
+        };
+        builder.Entity<IdentityRole>().HasData(roles);
     }
 }
